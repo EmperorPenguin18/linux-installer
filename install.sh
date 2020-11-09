9c59ed8808ea3916e2237b94f938377e4977a1a8
EmperorPenguin18 <60635017+EmperorPenguin18@users.noreply.github.com>
2020-11-08 20:47:45 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 #Checks before starting
 if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root" 
@@ -33,12 +35,12 @@ pacman -S dmidecode --noconfirm
 DISKNAME=$(lsblk | grep disk | awk '{print $1;}')
 DISKSIZE=$(lsblk --output SIZE -n -d /dev/$DISKNAME | sed 's/.$//')
 MEMSIZE=$(dmidecode -t 17 | grep "Size.*MB" | awk '{s+=$2} END {print s / 1024}')
-if [ $swap = "n" ]; then
+if [[ $swap = "n" ]]; then
    parted --script /dev/$DISKNAME \
       mklabel gpt \
       mkpart P1 fat32 1MB 261MB \
       set 1 esp on \
-      mkpart P2 btrfs 261MB $(echo $DISKSIZE)GB \
+      mkpart P2 btrfs 261MB $(echo $DISKSIZE)GB
 else
    parted --script /dev/$DISKNAME \
       mklabel gpt \
@@ -56,7 +58,7 @@ fi
 #Format partitions
 mkfs.fat -F32 /dev/$(echo $DISKNAME)1
 mkfs.btrfs -L arch /dev/$(echo $DISKNAME)2
-if [ $swap != "n" ]; then
+if [[ $swap != "n" ]]; then
    mkswap /dev/$(echo $DISKNAME)3
    swapon /dev/$(echo $DISKNAME)3
 fi
@@ -87,19 +89,19 @@ reflector --country Canada --protocol https --sort rate --save /etc/pacman.d/mir
 pacman -Sy
 
 #Install packages
-if [ $(cat /proc/cpuinfo | grep name | grep Intel | wc -l) -gt 0 ]; then cpu="intel"; else cpu="amd"; fi
+if [[ $(cat /proc/cpuinfo | grep name | grep Intel | wc -l) -gt 0 ]]; then cpu="intel"; else cpu="amd"; fi
 virtual=$(dmidecode -s system-product-name)
-if [ $virtual = "VirtualBox" ]; then
+if [[ $virtual = "VirtualBox" ]]; then
    virtual="virtualbox-guest-utils virtualbox-guest-dkms"
-elif [ $virtual = "KVM" ]; then
+elif [[ $virtual = "KVM" ]]; then
    virtual="qemu-guest-agent"
 else
    virtual=""
 fi
-if [ $distro = "debian" ]; then
+if [[ $distro = "debian" ]]; then
    pacman -S debootstrap
    #*
-elif [ $distro = "fedora" ]; then
+elif [[ $distro = "fedora" ]]; then
    chmod 777 ../
    echo "%nobody ALL=(ALL) NOPASSWD: /usr/bin/pacman" >> /etc/sudoers
    echo "%nobody ALL=(ALL) NOPASSWD: /usr/bin/makepkg" >> /etc/sudoers
@@ -111,7 +113,7 @@ elif [ $distro = "fedora" ]; then
    rm -r dnf
    cd linux-installer
    #*
-elif [ $distro = "void" ]; then
+elif [[ $distro = "void" ]]; then
    chmod 777 ../
    echo "%nobody ALL=(ALL) NOPASSWD: /usr/bin/pacman" >> /etc/sudoers
    echo "%nobody ALL=(ALL) NOPASSWD: /usr/bin/makepkg" >> /etc/sudoers
@@ -131,12 +133,12 @@ fi
 #Generate FSTAB
 UUID1=$(arch-chroot /mnt blkid -s UUID -o value /dev/$(echo $DISKNAME)1)
 UUID2=$(arch-chroot /mnt blkid -s UUID -o value /dev/$(echo $DISKNAME)2)
-if [ $swap != "n" ]; then
+if [[ $swap != "n" ]]; then
    UUID3=$(arch-chroot /mnt blkid -s UUID -o value /dev/$(echo $DISKNAME)3)
    echo UUID=$UUID3 none  swap  defaults 0  0 >> /mnt/etc/fstab
 fi
 echo UUID=$UUID1 /boot/EFI   vfat  rw,relatime,fmask=0022,dmask=0022,codepage=437,iocharset=iso8859-1,shortname=mixed,utf8,errors=remount-ro   0  2 >> /mnt/etc/fstab
-if [ $(lsblk -d -o name,rota | grep $DISKNAME | grep 1 | wc -l) -eq 1 ]; then
+if [[ $(lsblk -d -o name,rota | grep $DISKNAME | grep 1 | wc -l) -eq 1 ]]; then
    echo UUID=$UUID2 /  btrfs rw,relatime,compress=lzo,autodefrag,space_cache,subvol=/_active/rootvol   0  0 >> /mnt/etc/fstab
    echo UUID=$UUID2 /tmp  btrfs rw,relatime,compress=lzo,autodefrag,space_cache,subvol=_active/tmp  0  0 >> /mnt/etc/fstab
    echo UUID=$UUID2 /home btrfs rw,relatime,compress=lzo,autodefrag,space_cache,subvol=_active/homevol   0  0 >> /mnt/etc/fstab
@@ -170,11 +172,11 @@ echo "127.0.1.1   $(echo $host).localdomain  $host" >> /mnt/etc/hosts
 arch-chroot /mnt systemctl enable NetworkManager
 
 #Create root password
-arch-chroot /mnt echo $rpass | chpasswd --stdin
+arch-chroot /mnt echo root:$(echo $rpass) | chpasswd
 
 #Create user
 arch-chroot /mnt useradd -m $user
-arch-chroot /mnt echo $upass | chpasswd --stdin $user
+arch-chroot /mnt echo $(echo $user):$(echo $upass) | chpasswd
 arch-chroot /mnt usermod -aG audio,video,optical,storage $user
 arch-chroot /mnt echo "permit persist $user" > /etc/doas.conf
 
