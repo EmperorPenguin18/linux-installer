2099df15c0cbc212e86340e09117ab7f2c25fd2c
Sebastien MacDougall-Landry <60635017+EmperorPenguin18@users.noreply.github.com>
2020-11-27 17:43:56 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -226,7 +226,7 @@ elif [[ $distro = "fedora" ]]; then
    arch-chroot /media/loop dnf install -y --installroot=/mnt --releasever=33 --setopt=install_weak_deps=False --setopt=keepcache=True --nogpgcheck basesystem dnf glibc-langpack-en glibc-locale-source iputils NetworkManager
    #cp /etc/resolv.conf /mnt/etc/resolv.conf
    arch-chroot /mnt localedef -c -i en_US -f UTF-8 en_US-UTF-8
-   arch-chroot /mnt dnf install -y --setopt=install_weak_deps=False --setopt=keepcache=True $grub passwd linux-firmware efibootmgr os-prober btrfs-progs dosfstools $cpu git $virtual
+   arch-chroot /mnt dnf install -y --setopt=install_weak_deps=False --setopt=keepcache=True kernel $grub passwd linux-firmware efibootmgr os-prober btrfs-progs dosfstools $cpu git $virtual
    #*microcode?*
 elif [[ $distro = "void" ]]; then
    if [[ $(cat /proc/cpuinfo | grep name | grep Intel | wc -l) -gt 0 ]]; then cpu="iucode-tool intel-ucode"; else cpu="linux-firmware-amd"; fi
@@ -287,8 +287,6 @@ fi
 
 #Create root password
 printf "$rpass\n$rpass\n" | arch-chroot /mnt passwd
-
-if [[ $distro = "fedora" ]]; then arch-chroot /media/loop dnf --installroot=/mnt install -y kernel; fi
 
 #Create user
 arch-chroot /mnt useradd -m -s /bin/bash $user
