c1372e472fc115eea4360b6694d9e70d6f9ab4f9
Sebastien MacDougall-Landry <60635017+EmperorPenguin18@users.noreply.github.com>
2020-12-11 00:19:01 -0500

    Update fedora.sh

--- fedora.sh
+++ fedora.sh
@@ -1,43 +1,43 @@
 #!/bin/bash
 
-#          /:-------------:\
-#       :-------------------::
-#     :-----------/shhOHbmp---:\
-#   /-----------omMMMNNNMMD  ---:
-#  :-----------sMMMMNMNMP.    ---:
-# :-----------:MMMdP-------    ---\
-#,------------:MMMd--------    ---:
-#:------------:MMMd-------    .---:
-#:----    oNMMMMMMMMMNho     .----:
-#:--     .+shhhMMMmhhy++   .------/
-#:-    -------:MMMd--------------:
-#:-   --------/MMMd-------------;
-#:-    ------/hMMMy------------:
-#:-- :dMNdhhdNMMNo------------;
-#:---:sdNMMMMNds:------------:
-#:------:://:-------------::
-#:---------------------://
+PURPLE='\033[1;34m'
+WHITE='\033[1;37m'
+printf "          ${PURPLE}/:-------------:\\n"
+printf "       ${PURPLE}:-------------------::\n"
+printf "     ${PURPLE}:-----------${WHITE}/shhOHbmp${PURPLE}---:\\n"
+printf "   ${PURPLE}/-----------${WHITE}omMMMNNNMMD${PURPLE}  ---:\n"
+printf "  ${PURPLE}:-----------${WHITE}sMMMMNMNMP${PURPLE}.    ---:\n"
+printf " ${PURPLE}:-----------${WHITE}:MMMdP${PURPLE}-------    ---\\n"
+printf "${PURPLE},------------${WHITE}:MMMd${PURPLE}--------    ---:\n"
+printf "${PURPLE}:------------${WHITE}:MMMd${PURPLE}-------    .---:\n"
+printf "${PURPLE}:----    ${WHITE}oNMMMMMMMMMNho${PURPLE}     .----:\n"
+printf "${PURPLE}:--     .${WHITE}+shhhMMMmhhy++${PURPLE}   .------/\n"
+printf "${PURPLE}:-    -------${WHITE}:MMMd${PURPLE}--------------:\n"
+printf "${PURPLE}:-   --------${WHITE}/MMMd${PURPLE}-------------;\n"
+printf "${PURPLE}:-    ------${WHITE}/hMMMy${PURPLE}------------:\n"
+printf "${PURPLE}:-- ${WHITE}:dMNdhhdNMMNo${PURPLE}------------;\n"
+printf "${PURPLE}:---${WHITE}:sdNMMMMNds:${PURPLE}------------:\n"
+printf "${PURPLE}:------${WHITE}:://:${PURPLE}-------------::\n"
+printf "${PURPLE}:---------------------://\n"
 
 BOOTTYPE=$1
-time=$2
-host=$3
-pass=$4
-user=$5
-DISKNAME=$6
-virtual=$(dmidecode -s system-product-name)
-ROOTNAME=$7
+PASS=$2
+USER=$3
+DISKNAME=$4
+VIRTUAL=$(dmidecode -s system-product-name)
+ROOTNAME=$5
 
 #Set variables
-if [[ $(cat /proc/cpuinfo | grep name | grep Intel | wc -l) -gt 0 ]]; then cpu="iucode-tool"; fi
-if [[ $virtual = "KVM" ]]; then
-   virtual="qemu-guest-agent"
+if [[ $(cat /proc/cpuinfo | grep name | grep Intel | wc -l) -gt 0 ]]; then CPU="iucode-tool"; fi
+if [[ $VIRTUAL = "KVM" ]]; then
+   VIRTUAL="qemu-guest-agent"
 else
-   virtual=""
+   VIRTUAL=""
 fi
 if [[ $BOOTTYPE = "efi" ]]; then
-   grub=""
+   GRUB=""
 else
-   grub="grub2-pc"
+   GRUB="grub2-pc"
 fi
 
 #Get DNF
@@ -59,30 +59,20 @@ arch-chroot /media/loop dnf install -y --installroot=/mnt --releasever=33 --seto
 arch-chroot /mnt localedef -c -i en_US -f UTF-8 en_US-UTF-8
 
 #Install packages
-arch-chroot /mnt dnf install -y --setopt=install_weak_deps=False --setopt=keepcache=True kernel $grub passwd linux-firmware btrfs-progs dosfstools $cpu git $virtual cryptsetup-luks
-
-#Set time
-ln -sf /mnt/usr/share/zoneinfo/$(echo $time) /mnt/etc/localtime
-arch-chroot /mnt hwclock --systohc
+arch-chroot /mnt dnf install -y --setopt=install_weak_deps=False --setopt=keepcache=True kernel $GRUB passwd linux-firmware btrfs-progs dosfstools $CPU git $VIRTUAL cryptsetup-luks sudo
 
 #Network stuff
-echo $host > /mnt/etc/hostname
-echo "127.0.0.1   localhost" > /mnt/etc/hosts
-echo "::1   localhost" >> /mnt/etc/hosts
-echo "127.0.1.1   $(echo $host).localdomain  $host" >> /mnt/etc/hosts
 arch-chroot /mnt systemctl enable NetworkManager
 
-#Create root password
-printf "$pass\n$pass\n" | arch-chroot /mnt passwd
-
 #Create user
-arch-chroot /mnt useradd -m -s /bin/bash $user
-printf "$pass\n$pass\n" | arch-chroot /mnt passwd $user
+arch-chroot /mnt useradd -m -s /bin/bash -G wheel $USER
+echo "root ALL(ALL) ALL" > /mnt/etc/sudoers
+echo "%wheel ALL=(ALL) ALL" >> /mnt/etc/sudoers
 
 #Create encryption key
 arch-chroot /mnt dd if=/dev/urandom of=/keyfile bs=32 count=1
 arch-chroot /mnt chmod 600 /keyfile
-echo "$pass" | arch-chroot /mnt cryptsetup luksAddKey /dev/$ROOTNAME /keyfile
+echo "$PASS" | arch-chroot /mnt cryptsetup luksAddKey /dev/$ROOTNAME /keyfile
 echo "cryptroot UUID=$(blkid -s UUID -o value /dev/$ROOTNAME) none" > /mnt/etc/crypttab
 
 #Create bootloader
