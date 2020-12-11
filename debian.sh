6a3a4484f2f08325c55191dd4d42d59091573729
Sebastien MacDougall-Landry <60635017+EmperorPenguin18@users.noreply.github.com>
2020-12-11 00:18:57 -0500

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -1,41 +1,40 @@
 #!/bin/bash
 
-#       _,met$$$$$gg.
-#    ,g$$$$$$$$$$$$$$$P.
-#  ,g$$P"     """Y$$.".
-# ,$$P'              `$$$.
-#',$$P       ,ggs.     `$$b:
-#`d$$'     ,$P"'   .    $$$
-# $$P      d$'     ,    $$P
-# $$:      $$.   -    ,d$$'
-# $$;      Y$b._   _,d$P'
-# Y$$.    `.`"Y$$$$P"'
-# `$$b      "-.__
-#  `Y$$
-#   `Y$$.
-#     `$$b.
-#       `Y$$b.
-#          `"Y$b._
-#              `"""
+RED='\033[1;31m'
+printf "${RED}       _,met\$\$\$\$\$gg.\n"
+printf "${RED}    ,g\$\$\$\$\$\$\$\$\$\$\$\$\$\$\$P.\n"
+printf "${RED}  ,g\$\$P\"     \"\"\"Y\$\$.\".\n"
+printf "${RED} ,\$\$P'              `\$\$\$.\n"
+printf "${RED}',\$\$P       ,ggs.     `\$\$b:\n"
+printf "${RED}`d\$\$'     ,\$P\"'   .    \$\$\$\n"
+printf "${RED} \$\$P      d\$'     ,    \$\$P\n"
+printf "${RED} \$\$:      \$\$.   -    ,d\$\$'\n"
+printf "${RED} \$\$;      Y\$b._   _,d\$P'\n"
+printf "${RED} Y\$\$.    `.`\"Y\$\$\$\$P\"'\n"
+printf "${RED} `\$\$b      \"-.__\n"
+printf "${RED}  `Y\$\$\n"
+printf "${RED}   `Y\$\$.\n"
+printf "${RED}     `\$\$b.\n"
+printf "${RED}       `Y\$\$b.\n"
+printf "${RED}          `\"Y\$b._\n"
+printf "${RED}              `\"\"\"\n"
 
 BOOTTYPE=$1
-time=$2
-host=$3
-pass=$4
-user=$5
-DISKNAME=$6
-ROOTNAME=$7
+PASS=$2
+USER=$3
+DISKNAME=$4
+ROOTNAME=$5
 
 #Set variables
 if [[ $(cat /proc/cpuinfo | grep name | grep Intel | wc -l) -gt 0 ]]; then
-   cpu="iucode-tool intel"
+   CPU="iucode-tool intel"
 else
-   cpu="amd64"
+   CPU="amd64"
 fi
 if [[ $BOOTTYPE = "efi" ]]; then
-   grub=""
+   CPU=""
 else
-   grub="grub2"
+   CPU="grub2"
 fi
 
 #Install base system to mounted disk
@@ -53,35 +52,25 @@ echo "LANG=en_US.UTF-8" > /mnt/etc/locale.conf
 sed -e '/#/d' -i /mnt/etc/apt/sources.list && sed -e 's/main/main contrib non-free/' -i /mnt/etc/apt/sources.list
 echo 'deb http://deb.xanmod.org releases main' | tee /mnt/etc/apt/sources.list.d/xanmod-kernel.list && wget -qO - https://dl.xanmod.org/gpg.key | arch-chroot /mnt apt-key add -
 arch-chroot /mnt apt update
-arch-chroot /mnt apt install -y linux-xanmod-edge firmware-linux $grub btrfs-progs dosfstools $(echo $cpu)-microcode network-manager git cryptsetup
+arch-chroot /mnt 'DEBIAN_FRONTEND=noninteractive apt install -y linux-xanmod-edge firmware-linux $GRUB btrfs-progs dosfstools $(echo $CPU)-microcode network-manager git cryptsetup sudo'
 
 #Clean up install
 arch-chroot /mnt apt purge -y nano vim-common
 arch-chroot /mnt apt upgrade -y
-arch-chroot /mnt dpkg-reconfigure $(arch-chroot /mnt dpkg-query -l | grep linux-image | awk '{print $2}') $grub
-
-#Set time
-ln -sf /mnt/usr/share/zoneinfo/$(echo $time) /mnt/etc/localtime
-arch-chroot /mnt hwclock --systohc
+arch-chroot /mnt dpkg-reconfigure $(arch-chroot /mnt dpkg-query -l | grep linux-image | awk '{print $2}') $GRUB
 
 #Network stuff
-echo $host > /mnt/etc/hostname
-echo "127.0.0.1   localhost" > /mnt/etc/hosts
-echo "::1   localhost" >> /mnt/etc/hosts
-echo "127.0.1.1   $(echo $host).localdomain  $host" >> /mnt/etc/hosts
 arch-chroot /mnt systemctl enable NetworkManager
 
-#Create root password
-printf "$pass\n$pass\n" | arch-chroot /mnt passwd
-
 #Create user
-arch-chroot /mnt useradd -m -s /bin/bash $user
-printf "$pass\n$pass\n" | arch-chroot /mnt passwd $user
+arch-chroot /mnt useradd -m -s /bin/bash -G wheel $USER
+echo "root ALL(ALL) ALL" > /mnt/etc/sudoers
+echo "%wheel ALL=(ALL) ALL" >> /mnt/etc/sudoers
 
 #Create encryption key
 arch-chroot /mnt mkdir -m 0700 /etc/keys
 arch-chroot /mnt dd if=/dev/urandom bs=1 count=64 of=/etc/keys/root.key conv=excl,fsync
-echo "$pass" | arch-chroot /mnt cryptsetup luksAddKey /dev/$ROOTNAME /etc/keys/root.key
+echo "$PASS" | arch-chroot /mnt cryptsetup luksAddKey /dev/$ROOTNAME /etc/keys/root.key
 echo "cryptroot UUID=$(blkid -s UUID -o value /dev/$ROOTNAME) /etc/keys/root.key luks,discard,key-slot=1" > /mnt/etc/crypttab
 echo "KEYFILE_PATTERN=\"/etc/keys/*.key\"" >> /mnt/etc/cryptsetup-initramfs/conf-hook
 echo "UMASK=0077" >> /mnt/etc/initramfs-tools/initramfs.conf
