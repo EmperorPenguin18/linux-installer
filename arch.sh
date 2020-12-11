45101f5011a5c20d0b4ec8778796de88395a5db1
Sebastien MacDougall-Landry <60635017+EmperorPenguin18@users.noreply.github.com>
2020-12-11 00:18:48 -0500

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -1,54 +1,51 @@
 #!/bin/bash
 
-#                   -`
-#                  .o+`
-#                 `ooo/
-#                `+oooo:
-#               `+oooooo:
-#               -+oooooo+:
-#             `/:-:++oooo+:
-#            `/++++/+++++++:
-#           `/++++++++++++++:
-#          `/+++ooooooooooooo/`
-#         ./ooosssso++osssssso+`
-#        .oossssso-````/ossssss+`
-#       -osssssso.      :ssssssso.
-#      :osssssss/        osssso+++.
-#     /ossssssss/        +ssssooo/-
-#   `/ossssso+/:-        -:/+osssso+-
-#  `+sso+:-`                 `.-/+oso:
-# `++:.                           `-/+/
-# .`                                 `/
+LCYAN='\033[1;36m'
+printf "${LCYAN}                   -`\n"
+printf "${LCYAN}                  .o+`\n"
+printf "${LCYAN}                 `ooo/\n"
+printf "${LCYAN}                `+oooo:\n"
+printf "${LCYAN}               `+oooooo:\n"
+printf "${LCYAN}               -+oooooo+:\n"
+printf "${LCYAN}             `/:-:++oooo+:\n"
+printf "${LCYAN}            `/++++/+++++++:\n"
+printf "${LCYAN}           `/++++++++++++++:\n"
+printf "${LCYAN}          `/+++ooooooooooooo/`\n"
+printf "${LCYAN}         ./ooosssso++osssssso+`\n"
+printf "${LCYAN}        .oossssso-````/ossssss+`\n"
+printf "${LCYAN}       -osssssso.      :ssssssso.\n"
+printf "${LCYAN}      :osssssss/        osssso+++.\n"
+printf "${LCYAN}     /ossssssss/        +ssssooo/-\n"
+printf "${LCYAN}   `/ossssso+/:-        -:/+osssso+-\n"
+printf "${LCYAN}  `+sso+:-`                 `.-/+oso:\n"
+printf "${LCYAN} `++:.                           `-/+/\n"
+printf "${LCYAN} .`                                 `/\n"
 
 BOOTTYPE=$1
-time=$2
-host=$3
-pass=$4
-user=$5
-DISKNAME=$6
-virtual=$(dmidecode -s system-product-name)
-ROOTNAME=$7
+PASS=$2
+USER=$3
+DISKNAME=$4
+VIRTUAL=$(dmidecode -s system-product-name)
+ROOTNAME=$5
 
 #Set variables
 if [[ $(cat /proc/cpuinfo | grep name | grep Intel | wc -l) -gt 0 ]]; then
-  cpu="iucode-tool intel"
+  CPU="iucode-tool intel"
 else
-  cpu="amd"
+  CPU="amd"
 fi
-if [[ $virtual = "VirtualBox" ]]; then
-  virtual="virtualbox-guest-utils virtualbox-guest-dkms"
-elif [[ $virtual = "KVM" ]]; then
-  virtual="qemu-guest-agent"
+if [[ $VIRTUAL = "VirtualBox" ]]; then
+  VIRTUAL="virtualbox-guest-utils virtualbox-guest-dkms"
+elif [[ $VIRTUAL = "KVM" ]]; then
+  VIRTUAL="qemu-guest-agent"
 else
-  virtual=""
+  VIRTUAL=""
 fi
 
 #Install base system + packages
-pacstrap /mnt base linux-zen linux-zen-headers linux-firmware grub grub-btrfs efibootmgr os-prober btrfs-progs dosfstools $(echo $cpu)-ucode opendoas networkmanager git $virtual
+pacstrap /mnt base linux-zen linux-zen-headers linux-firmware grub grub-btrfs efibootmgr os-prober btrfs-progs dosfstools $(echo $CPU)-ucode opendoas networkmanager git $VIRTUAL
 
 #Set localization stuff
-ln -sf /mnt/usr/share/zoneinfo/$(echo $time) /mnt/etc/localtime
-arch-chroot /mnt hwclock --systohc
 echo "en_US.UTF-8 UTF-8" > /mnt/etc/locale.gen
 arch-chroot /mnt locale-gen
 echo "LANG=en_US.UTF-8" > /mnt/etc/locale.conf
@@ -56,8 +53,8 @@ echo "LANG=en_US.UTF-8" > /mnt/etc/locale.conf
 #Create encryption key
 arch-chroot /mnt dd bs=512 count=4 if=/dev/random of=/crypto_keyfile.bin iflag=fullblock
 arch-chroot /mnt chmod 600 /crypto_keyfile.bin
-arch-chroot /mnt chmod 600 /boot/initramfs-linux*
-echo "$pass" | arch-chroot /mnt cryptsetup luksAddKey /dev/$ROOTNAME /crypto_keyfile.bin
+arch-chroot /mnt chmod 600 /boot/$(ls /mnt/boot | grep initramfs-linux)
+echo "$PASS" | arch-chroot /mnt cryptsetup luksAddKey /dev/$ROOTNAME /crypto_keyfile.bin
 
 #Setup initramfs HOOKS
 echo "MODULES=()" > /mnt/etc/mkinitcpio.conf
@@ -67,25 +64,26 @@ echo "HOOKS=(base udev encrypt autodetect modconf block btrfs filesystems keyboa
 arch-chroot /mnt mkinitcpio -P
 
 #Network stuff
-echo $host > /mnt/etc/hostname
-echo "127.0.0.1   localhost" > /mnt/etc/hosts
-echo "::1   localhost" >> /mnt/etc/hosts
-echo "127.0.1.1   $(echo $host).localdomain  $host" >> /mnt/etc/hosts
 arch-chroot /mnt systemctl enable NetworkManager
 
-#Create root password
-printf "$pass\n$pass\n" | arch-chroot /mnt passwd
-
 #Create user
-arch-chroot /mnt useradd -m -s /bin/bash $user
-printf "$pass\n$pass\n" | arch-chroot /mnt passwd $user
-echo "permit persist $user" > /mnt/etc/doas.conf
+arch-chroot /mnt useradd -m -s /bin/bash $USER
+
+#Configure doas
+echo "#This system uses doas instead of sudo" > /mnt/etc/doas.conf
+echo "permit persist $USER" >> /mnt/etc/doas.conf
+arch-chroot /mnt su sebastien -c "git clone https://aur.archlinux.org/opendoas-sudo.git"
+mv /mnt/opendoas-sudo/* /mnt
+arch-chroot /mnt su sebastien -c "makepkg --noconfirm"
+arch-chroot /mnt pacman -U opendoas*
+rm -r /mnt/PKGBUILD /mnt/pkg /mnt/src /mnt/opendoas*
+arch-chroot /mnt ln -s /etc/doas.conf /etc/sudoers
 
 #Create bootloader
 sed -i 's/#GRUB_ENABLE_CRYPTODISK=y/GRUB_ENABLE_CRYPTODISK=y/g' /mnt/etc/default/grub
 sed -i "s/GRUB_CMDLINE_LINUX=\"\"/GRUB_CMDLINE_LINUX=\"cryptdevice=UUID=$(blkid -s UUID -o value /dev/$ROOTNAME):cryptroot\"/g" /mnt/etc/default/grub
 if [[ $BOOTTYPE = "efi" ]]; then
-   arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB --recheck
+   arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB --recheck
 else
    arch-chroot /mnt grub-install /dev/$DISKNAME
 fi
