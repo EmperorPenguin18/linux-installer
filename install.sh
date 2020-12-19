cc13dee77dd0e73decb464f7b77e4fc2f4da6a7b
Sebastien MacDougall-Landry <60635017+EmperorPenguin18@users.noreply.github.com>
2020-12-19 18:45:32 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -26,27 +26,36 @@ user_prompts ()
 {
    mv /usr/share/zoneinfo/right /usr/share/right
    mv /usr/share/zoneinfo/posix /usr/share/posix
-   echo "-------------------------------------------------"
-   echo "           Welcome to linux-installer!           "
-   echo "-------------------------------------------------"
-   echo "Please answer the following questions to begin:"
-   echo
-   DISKNAME=$(lsblk | awk '/disk/ {print $1 " " $4}' | fzf -i --prompt "Choose disk to install to. >" --layout reverse | awk '{print $1;}')
-   clear
-   DISTRO=$(printf "Arch\nDebian\nFedora\nVoid\n" | fzf -i --prompt "What distro do you want to install? >" --layout reverse | awk '{print tolower($0)}')
-   clear
-   TIME=$(find /usr/share/zoneinfo -type f | sed 's|/usr/share/zoneinfo/||' | fzf -i --prompt "Choose a timezone. >" --layout reverse)
-   clear
-   read -r -p "What will the hostname of this computer be? >" HOST
-   read -r -p "Enter your username. >" USER
-   read -r -s -p "Enter your password. >" PASS
-   echo
-   read -r -s -p "Confirm password. >" PASSCHECK
-   echo
-   if [ "${PASS}" != "${PASSCHECK}" ]; then echo "Passwords do not match"; exit 1; fi
-   read -r -p "Do you want hibernation enabled (Swap partition) [Y/n] " SWAP
-   read -r -p "This will delete all data on selected storage device. Are you sure you want to continue? [y/N] " SURE
-   if [ "${SURE}" != "y" ]; then exit 1; fi
+   lsblk | awk '/disk/ {print $1 " " $4 " off"}' > disks.txt
+   find /usr/share/zoneinfo -type f | sed 's|/usr/share/zoneinfo/||' | sed -e 's/$/ "" off/' > zones.txt
+   TEMP=$(dialog --stdout \
+      --msgbox "Welcome to linux-installer! Please answer the following questions to begin." 0 0 \
+      --and-widget --clear --radiolist "Choose disk to install to." 0 0 $(wc -l < disks.txt) --file disks.txt \
+      --and-widget --clear --radiolist "What distro do you want to install?" 0 0 0 arch "" on debian "" off fedora "" off void "" off \
+      --and-widget --clear --radiolist "Choose a timezone." 0 0 $(wc -l < zones.txt) --file zones.txt \
+      --and-widget --clear --inputbox "What will the hostname of this computer be?" 0 0 \
+      --and-widget --clear --inputbox "Enter your username." 0 0 \
+      --and-widget --clear --passwordbox "Enter your password." 0 0 \
+      --and-widget --clear --passwordbox "Confirm password." 0 0 \
+   )
+   rm disks.txt zones.txt
+   if [ "$(awk '{print $6}')" != "$(awk '{print $7}')" ]; then echo "Passwords do not match"; exit 1; fi
+   DISKNAME=$(echo $TEMP | awk '{print $1}')
+   DISTRO=$(echo $TEMP | awk '{print $2}')
+   TIME=$(echo $TEMP | awk '{print $3}')
+   HOST=$(echo $TEMP | awk '{print $4}')
+   USER=$(echo $TEMP | awk '{print $5}')
+   PASS=$(echo $TEMP | awk '{print $6}')
+   if dialog --yesno "Do you want hibernation enabled (Swap partition)" 0 0; then
+      SWAP=y
+   else
+      SWAP=n
+   fi
+   if dialog --default-button "no" --yesno "This will delete all data on selected storage device. Are you sure you want to continue?" 0 0; then
+      SURE=y
+   else
+      exit 1
+   fi
    clear
 }
 
