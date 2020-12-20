25788c1e4b92d58e9a7da1778848cc4a548420c5
Sebastien MacDougall-Landry <60635017+EmperorPenguin18@users.noreply.github.com>
2020-12-19 22:08:49 -0500

    Update void.sh

--- void.sh
+++ void.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+check_error ()
+{
+   if [ $? -ne 0]; then
+      echo $1
+      exit -1
+   fi
+}
+
 LGREEN='\033[1;32m'
 DGRAY='\033[1;30m'
 NC='\033[0m'
@@ -50,64 +58,98 @@ fi
 
 #Install the base system
 cp /mnt/etc/fstab fstab.bak
+check_error
 wget https://alpha.us.repo.voidlinux.org/live/current/$(curl -s https://alpha.us.repo.voidlinux.org/live/current/ | grep void-x86_64-ROOTFS | cut -d '"' -f 2)
+check_error
 tar xvf void-x86_64-ROOTFS-*.tar.xz -C /mnt
+check_error
 mv fstab.bak /mnt/etc/fstab
+check_error
 echo "repository=https://alpha.us.repo.voidlinux.org/current" > /mnt/etc/xbps.d/xbps.conf
+check_error
 echo "repository=https://alpha.us.repo.voidlinux.org/current/nonfree" >> /mnt/etc/xbps.d/xbps.conf
 echo "repository=https://alpha.us.repo.voidlinux.org/current/multilib" >> /mnt/etc/xbps.d/xbps.conf
 echo "repository=https://alpha.us.repo.voidlinux.org/current/multilib/nonfree" >> /mnt/etc/xbps.d/xbps.conf
 echo "ignorepkg=sudo" >> /mnt/etc/xbps.d/xbps.conf
 echo "ignorepkg=dracut" >> /mnt/etc/xbps.d/xbps.conf
 ln -s /mnt/etc/sv/dhcpcd /mnt/etc/runit/runsvdir/default/
+check_error
 arch-chroot /mnt dhcpcd
+check_error
 arch-chroot /mnt xbps-install -Suy xbps
+check_error
 arch-chroot /mnt xbps-install -uy
+check_error
 arch-chroot /mnt xbps-install -y base-system
+check_error
 arch-chroot /mnt xbps-remove -y base-voidstrap sudo
+check_error
 rm void-x86_64-ROOTFS-*.tar.xz
+check_error
 
 #Install packages
 arch-chroot /mnt xbps-install -Sy linux linux-firmware mkinitcpio mkinitcpio-encrypt mkinitcpio-udev $GRUB grub-btrfs efibootmgr os-prober btrfs-progs dosfstools $CPU opendoas NetworkManager git $VIRTUAL cryptsetup fish-shell
+check_error
 arch-chroot /mnt xbps-reconfigure -fa
+check_error
 
 #Set localization stuff
 echo "en_US.UTF-8 UTF-8" > /mnt/etc/default/libc-locales
+check_error
 arch-chroot /mnt xbps-reconfigure -f glibc-locales
+check_error
 echo "LANG=en_US.UTF-8" > /mnt/etc/locale.conf
+check_error
 
 #Network stuff
 ln -s /mnt/etc/sv/NetworkManager /mnt/etc/runit/runsvdir/default/
+check_error
 
 #Create user
 arch-chroot /mnt useradd -m -s /bin/fish $USER
+check_error
 mkdir /mnt/home/$USER/.snapshots
+check_error
 
 #Configure doas
 echo "#This system uses doas instead of sudo" > /mnt/etc/doas.conf
+check_error
 echo "permit persist $USER" >> /mnt/etc/doas.conf
 ln -sf /mnt/usr/bin/doas /mnt/usr/bin/sudo
+check_error
 ln -s /mnt/etc/doas.conf /mnt/etc/sudoers
+check_error
 
 #Create encryption key
 dd bs=512 count=4 if=/dev/random of=/mnt/crypto_keyfile.bin iflag=fullblock
+check_error
 chmod 600 /mnt/crypto_keyfile.bin
+check_error
 chmod 600 /mnt/boot/initramfs*
+check_error
 echo "$PASS" | arch-chroot /mnt cryptsetup luksAddKey /dev/$ROOTNAME /crypto_keyfile.bin
+check_error
 
 #Setup initramfs HOOKS
 echo "MODULES=()" > /mnt/etc/mkinitcpio.conf
+check_error
 echo "BINARIES=()" >> /mnt/etc/mkinitcpio.conf
 echo "FILES=(/crypto_keyfile.bin)" >> /mnt/etc/mkinitcpio.conf
 echo "HOOKS=(base udev encrypt autodetect modconf block filesystems keyboard fsck)" >> /mnt/etc/mkinitcpio.conf
 arch-chroot /mnt mkinitcpio -c /etc/mkinitcpio.conf -g /boot/initramfs-$(ls /mnt/usr/lib/modules).img -k $(ls /mnt/usr/lib/modules)
+check_error
 
 #Create bootloader
 echo "GRUB_ENABLE_CRYPTODISK=y" >> /mnt/etc/default/grub
+check_error
 echo "GRUB_CMDLINE_LINUX=\"cryptdevice=UUID=$(blkid -s UUID -o value /dev/$ROOTNAME):cryptroot\"" >> /mnt/etc/default/grub
+check_error
 if [ "${BOOTTYPE}" = "efi" ]; then
    arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB --recheck
+   check_error
 else
    arch-chroot /mnt grub-install /dev/$DISKNAME
+   check_error
 fi
 arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
+check_error
