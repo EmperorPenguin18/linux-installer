731c342058a576508aef7450af3d1c9d2bea50d3
Sebastien MacDougall-Landry <60635017+EmperorPenguin18@users.noreply.github.com>
2021-12-02 18:21:56 -0500

    Update arch.sh

--- distros/arch.sh
+++ distros/arch.sh
@@ -59,7 +59,7 @@ set_locale ()
 set_initramfs ()
 {
    arch-chroot /mnt su $USER -c "git clone https://aur.archlinux.org/mkinitcpio-openswap.git /home/$USER/mkinitcpio-openswap" && \
-   mv /mnt/home/$USER/mkinitcpio/* /mnt && \
+   mv /mnt/home/$USER/mkinitcpio-openswap/* /mnt && \
    sed -i "s|2788eb78-074d-4424-9f1d-ebffc9c37262|$(blkid -s UUID -o value /dev/$(echo $DISKNAME2)3)|g" /mnt/openswap.conf && \
    sed -i 's|etc/keyfile-cryptswap|etc/keys/keyfile.bin|g' /mnt/openswap.conf && \
    chmod 777 /mnt && \
