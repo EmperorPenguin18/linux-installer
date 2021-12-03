99c6b7369749c236d297505c627632b800a6e789
Sebastien MacDougall-Landry <60635017+EmperorPenguin18@users.noreply.github.com>
2021-12-03 16:55:01 -0500

    Update arch.sh

--- distros/arch.sh
+++ distros/arch.sh
@@ -58,21 +58,27 @@ set_locale ()
 
 set_initramfs ()
 {
-   arch-chroot /mnt su $USER -c "git clone https://aur.archlinux.org/mkinitcpio-openswap.git /home/$USER/mkinitcpio-openswap" && \
-   mv /mnt/home/$USER/mkinitcpio-openswap/* /mnt && \
-   
-   chmod 777 /mnt && \
-   arch-chroot /mnt su $USER -c "makepkg --noconfirm" && \
-   arch-chroot /mnt pacman -U $(ls /mnt | grep mkinitcpio) --noconfirm && \
-   chmod 755 /mnt && \
-   rm -r /mnt/PKGBUILD /mnt/pkg /mnt/src /mnt/mkinitcpio* /mnt/openswap* /mnt/usage.install /mnt/LICENSE /mnt/home/$USER/mkinitcpio-openswap && \
-   sed -i "s|2788eb78-074d-4424-9f1d-ebffc9c37262|$(blkid -s UUID -o value /dev/$(echo $DISKNAME2)3)|g" /mnt/etc/openswap.conf && \
-   sed -i 's|etc/keyfile-cryptswap|etc/keys/keyfile.bin|g' /mnt/etc/openswap.conf && \
-   sed -i 's|#keyfile_device_mount_options="--options=subvol=__active/__"|keyfile_device_mount_options="--options=subvol=_active/rootvol"|g' /mnt/etc/openswap.conf && \
-   echo "MODULES=()" > /mnt/etc/mkinitcpio.conf && \
-   echo "BINARIES=()" >> /mnt/etc/mkinitcpio.conf && \
-   echo "FILES=(/etc/keys/keyfile.bin)" >> /mnt/etc/mkinitcpio.conf && \
-   echo "HOOKS=(base udev encrypt openswap autodetect modconf block filesystems keyboard resume fsck)" >> /mnt/etc/mkinitcpio.conf && \
+   echo "MODULES=()" > /mnt/etc/mkinitcpio.conf
+   echo "BINARIES=()" >> /mnt/etc/mkinitcpio.conf
+   echo "FILES=(/etc/keys/keyfile.bin)" >> /mnt/etc/mkinitcpio.conf
+   if [ "${SWAP}" != "n" ]
+   then
+      arch-chroot /mnt su $USER -c "git clone https://aur.archlinux.org/mkinitcpio-openswap.git /home/$USER/mkinitcpio-openswap" && \
+      mv /mnt/home/$USER/mkinitcpio-openswap/* /mnt && \
+      chmod 777 /mnt && \
+      arch-chroot /mnt su $USER -c "makepkg --noconfirm" && \
+      arch-chroot /mnt pacman -U $(ls /mnt | grep mkinitcpio) --noconfirm && \
+      chmod 755 /mnt && \
+      rm -r /mnt/PKGBUILD /mnt/pkg /mnt/src /mnt/mkinitcpio* /mnt/openswap* /mnt/usage.install /mnt/LICENSE /mnt/home/$USER/mkinitcpio-openswap && \
+      sed -i "s|2788eb78-074d-4424-9f1d-ebffc9c37262|$(blkid -s UUID -o value /dev/$(echo $DISKNAME2)3)|g" /mnt/etc/openswap.conf && \
+      sed -i 's|etc/keyfile-cryptswap|etc/keys/keyfile.bin|g' /mnt/etc/openswap.conf && \
+      sed -i 's|#keyfile_device_mount_options="--options=subvol=__active/__"|keyfile_device_mount_options="--options=subvol=_active/rootvol"|g' /mnt/etc/openswap.conf && \
+      echo "HOOKS=(base udev encrypt openswap autodetect modconf block filesystems keyboard resume fsck)" >> /mnt/etc/mkinitcpio.conf || \
+      return 1
+   else
+      echo "HOOKS=(base udev encrypt autodetect modconf block filesystems keyboard resume fsck)" >> /mnt/etc/mkinitcpio.conf || \
+      return 1
+   fi
    arch-chroot /mnt mkinitcpio -P && \
    chmod 600 /mnt/boot/initramfs-linux* || \
    return 1
@@ -97,7 +103,7 @@ create_user ()
 create_bootloader ()
 {
    sed -i 's/#GRUB_ENABLE_CRYPTODISK=y/GRUB_ENABLE_CRYPTODISK=y/g' /mnt/etc/default/grub
-   sed -i "s/GRUB_CMDLINE_LINUX=\"\"/GRUB_CMDLINE_LINUX=\"cryptkey=rootfs:/etc/keys/keyfile.bin cryptdevice=UUID=$(blkid -s UUID -o value /dev/$ROOTNAME):cryptroot\"/g" /mnt/etc/default/grub || \
+   sed -i "s/GRUB_CMDLINE_LINUX=\"\"/GRUB_CMDLINE_LINUX=\"cryptkey=rootfs:\/etc\/keys\/keyfile.bin cryptdevice=UUID=$(blkid -s UUID -o value /dev/$ROOTNAME):cryptroot\"/g" /mnt/etc/default/grub || \
    return 1
    if [ "${BOOTTYPE}" = "efi" ]; then
       arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB --recheck || \
