2f6d9d9d7f9150ac13a7b340488f78916ebd93ad
Sebastien MacDougall-Landry <60635017+EmperorPenguin18@users.noreply.github.com>
2022-01-16 17:46:35 -0500

    Update fedora.sh

--- distros/fedora.sh
+++ distros/fedora.sh
@@ -8,23 +8,23 @@ print_logo ()
    PURPLE='\033[1;34m'
    WHITE='\033[1;37m'
    NC='\033[0m'
-   printf "          ${PURPLE}/:-------------:\\n"
-   printf "       ${PURPLE}:-------------------::\n"
-   printf "     ${PURPLE}:-----------${WHITE}/shhOHbmp${PURPLE}---:\\n"
-   printf "   ${PURPLE}/-----------${WHITE}omMMMNNNMMD${PURPLE}  ---:\n"
-   printf "  ${PURPLE}:-----------${WHITE}sMMMMNMNMP${PURPLE}.    ---:\n"
-   printf " ${PURPLE}:-----------${WHITE}:MMMdP${PURPLE}-------    ---\\n"
-   printf "${PURPLE},------------${WHITE}:MMMd${PURPLE}--------    ---:\n"
-   printf "${PURPLE}:------------${WHITE}:MMMd${PURPLE}-------    .---:\n"
-   printf "${PURPLE}:----    ${WHITE}oNMMMMMMMMMNho${PURPLE}     .----:\n"
-   printf "${PURPLE}:--     .${WHITE}+shhhMMMmhhy++${PURPLE}   .------/\n"
-   printf "${PURPLE}:-    -------${WHITE}:MMMd${PURPLE}--------------:\n"
-   printf "${PURPLE}:-   --------${WHITE}/MMMd${PURPLE}-------------;\n"
-   printf "${PURPLE}:-    ------${WHITE}/hMMMy${PURPLE}------------:\n"
-   printf "${PURPLE}:-- ${WHITE}:dMNdhhdNMMNo${PURPLE}------------;\n"
-   printf "${PURPLE}:---${WHITE}:sdNMMMMNds:${PURPLE}------------:\n"
-   printf "${PURPLE}:------${WHITE}:://:${PURPLE}-------------::\n"
-   printf "${PURPLE}:---------------------://\n${NC}" || \
+   printf "          ${PURPLE}/:-------------:\
+       ${PURPLE}:-------------------::
+     ${PURPLE}:-----------${WHITE}/shhOHbmp${PURPLE}---:\
+   ${PURPLE}/-----------${WHITE}omMMMNNNMMD${PURPLE}  ---:
+  ${PURPLE}:-----------${WHITE}sMMMMNMNMP${PURPLE}.    ---:
+ ${PURPLE}:-----------${WHITE}:MMMdP${PURPLE}-------    ---\
+${PURPLE},------------${WHITE}:MMMd${PURPLE}--------    ---:
+${PURPLE}:------------${WHITE}:MMMd${PURPLE}-------    .---:
+${PURPLE}:----    ${WHITE}oNMMMMMMMMMNho${PURPLE}     .----:
+${PURPLE}:--     .${WHITE}+shhhMMMmhhy++${PURPLE}   .------/
+${PURPLE}:-    -------${WHITE}:MMMd${PURPLE}--------------:
+${PURPLE}:-   --------${WHITE}/MMMd${PURPLE}-------------;
+${PURPLE}:-    ------${WHITE}/hMMMy${PURPLE}------------:
+${PURPLE}:-- ${WHITE}:dMNdhhdNMMNo${PURPLE}------------;
+${PURPLE}:---${WHITE}:sdNMMMMNds:${PURPLE}------------:
+${PURPLE}:------${WHITE}:://:${PURPLE}-------------::
+${PURPLE}:---------------------://${NC}\n" || \
    return 1
 }
 
@@ -47,15 +47,17 @@ install_packages ()
    else
       GRUB="grub2-pc"
    fi
+   [ -f /etc/yum.repos.d/fedora.repo ] && mv /etc/yum.repos.d/fedora.repo /etc/yum.repos.d/fedora.repo.bak
    NUM=$(curl -sL https://mirror.csclub.uwaterloo.ca/pub/fedora/linux/releases/ | cut -d '>' -f 2 | cut -d '/' -f 1 | sed '1,4d' | head -n -3 | sort -g | tail -1) && \
    pacman -S dnf --noconfirm --needed && \
-   mkdir /etc/yum.repos.d && \
+   mkdir -p /etc/yum.repos.d && \
    printf '[fedora]\nname=Fedora $releasever - $basearch\n#baseurl=http://download.example/pub/fedora/linux/releases/$releasver/Everything/$basearch/os\nmetalink=https://mirrors.fedoraproject.org/metalink?repo=fedora-$releasever&arch=$basearch\nenabled=1\ncountme=1\nmetadata_expire=7d\nrepo_gpgcheck=0\ntype=rpm\ngpgcheck=1\ngpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-fedora-$releasever-$basearch\nskip_if_unavailable=False' > /etc/yum.repos.d/fedora.repo && \
    dnf install -y --installroot=/mnt --releasever=$NUM --setopt=install_weak_deps=False --setopt=keepcache=True --nogpgcheck basesystem dnf glibc-langpack-en glibc-locale-source iputils NetworkManager && \
    sed -i '$s|^|PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin |' /usr/bin/arch-chroot && \
    set_locale && \
    arch-chroot /mnt dnf install -y --setopt=install_weak_deps=False --setopt=keepcache=True kernel os-prober $GRUB passwd linux-firmware btrfs-progs dosfstools $CPU microcode_ctl git $VIRTUAL cryptsetup sudo fish || \
    return 1
+   [ -f /etc/yum.repos.d/fedora.repo.bak ] && mv /etc/yum.repos.d/fedora.repo.bak /etc/yum.repos.d/fedora.repo
 }
 
 create_user ()
