0d963af96986133a42b6a2061fcf150da64e8302
EmperorPenguin18 <60635017+EmperorPenguin18@users.noreply.github.com>
2024-10-20 10:59:04 -0400

    major refresh

--- install.sh
+++ install.sh
@@ -1,230 +1,276 @@
+#!/bin/sh
+
 #linux-installer by Sebastien MacDougall-Landry
 #License is available at
 #https://github.com/EmperorPenguin18/linux-installer/blob/main/LICENSE
-#!/bin/sh
 
-pre_checks ()
-{
-   timedatectl set-ntp true
-   clear
-   if [ "$(id -u)" -ne 0 ]; then
-      echo "This script must be run as root" 
-      exit 1
-   fi
-   if [ "$(ls /usr/bin | grep pacman | wc -l)" -lt 1 ]; then
-      echo "This is not an Arch system"
-      exit 1
-   fi
-   if ping -q -c 1 -W 1 google.com >/dev/null; then
-      echo "The network is up"
-   else
-      echo "The network is down"
-      exit 1
-   fi
-   pacman -Sy >/dev/null 2>&1
-   pacman -Q | awk '{print $1}' > pre.txt
-   pacman -S dmidecode parted dosfstools util-linux reflector arch-install-scripts efibootmgr dialog wget cryptsetup bc --noconfirm --needed >/dev/null 2>&1 
-   export PS4='+(${BASH_SOURCE}:${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
-   set -x
-}
+######################################################################
+######################### Library functions ##########################
+######################################################################
 
-setup_partitions ()
-{
-   echo "Wiping all data on disk..."
-   dd if=/dev/zero of=/dev/$DISKNAME bs=4096 status=progress
-   if [ "$(efibootmgr | wc -l)" -gt 0 ]; then
-      BOOTTYPE="efi"
-   else
-      BOOTTYPE="legacy"
-      echo "Using legacy boot"
-   fi
-   DISKSIZE=$(lsblk --output SIZE -n -d -b /dev/$DISKNAME)
-   DISKSIZE=$(printf "$DISKSIZE / 1024 / 1024\n" | bc)
-   MEMSIZE=$(awk '/MemTotal/ {print $2}' /proc/meminfo)
-   MEMSIZE=$(printf "$MEMSIZE * 1.5 / 1024\n" | bc)
-   if [ "$(expr length $DISKNAME)" -eq 3 ]; then
-      DISKNAME2=$DISKNAME
-   else
-      DISKNAME2=$(echo $DISKNAME)p
-   fi
-   ROOTNAME=$(echo $DISKNAME2)2
-}
-
-partition_drive ()
-{
-   if [ "${SWAP}" = "n" ] && [ "${BOOTTYPE}" = "efi" ]; then
-      parted --script /dev/$DISKNAME \
-         mklabel gpt \
-         mkpart boot fat32 1MB 261MB \
-         set 1 esp on \
-         mkpart root btrfs 261MB $(echo $DISKSIZE)MB || \
-      return 1
-   elif [ "${SWAP}" = "n" ]; then
-      parted --script /dev/$DISKNAME \
-         mklabel msdos \
-         mkpart primary fat32 1MB 261MB \
-         set 1 boot on \
-         mkpart primary btrfs 261MB $(echo $DISKSIZE)MB || \
-      return 1
-   elif [ "${BOOTTYPE}" = "efi" ]; then
-      parted --script /dev/$DISKNAME \
-         mklabel gpt \
-         mkpart boot fat32 1MB 261MB \
-         set 1 esp on \
-         mkpart root btrfs 261MB $(expr $DISKSIZE - $MEMSIZE)MB \
-         mkpart swap linux-swap $(expr $DISKSIZE - $MEMSIZE)MB $(echo $DISKSIZE)MB || \
-      return 1
-   else
-      parted --script /dev/$DISKNAME \
-         mklabel msdos \
-         mkpart primary fat32 1MB 261MB \
-         set 1 boot on \
-         mkpart primary btrfs 261MB $(expr $DISKSIZE - $MEMSIZE)MB \
-         mkpart primary linux-swap $(expr $DISKSIZE - $MEMSIZE)MB $(echo $DISKSIZE)MB || \
-      return 1
-   fi
-}
-
-encrypt_partitions ()
-{
-   echo "$PASS" | cryptsetup -q luksFormat --type luks1 /dev/$ROOTNAME && \
-   echo "$PASS" | cryptsetup -q luksFormat --type luks1 /dev/$(echo $DISKNAME2)3 &&\
-   echo "$PASS" | cryptsetup open /dev/$ROOTNAME cryptroot && \
-   echo "$PASS" | cryptsetup open /dev/$(echo $DISKNAME2)3 cryptswap || \
-   return 1
-}
-
-format_partitions ()
-{
-   mkfs.fat -F 32 /dev/$(echo $DISKNAME2)1 && \
-   mkfs.btrfs /dev/mapper/cryptroot && \
-   mount /dev/mapper/cryptroot /mnt || \
-   return 1
-   if [ "${SWAP}" != "n" ]; then
-      mkswap /dev/mapper/cryptswap && \
-      swapon /dev/mapper/cryptswap || \
-      return 1
-   fi
-}
-
-mount_subvolumes ()
-{
-   pth=$(pwd) && \
-   cd /mnt && \
-   btrfs subvolume create _active && \
-   btrfs subvolume create _active/rootvol && \
-   btrfs subvolume create _active/homevol && \
-   btrfs subvolume create _active/tmp && \
-   btrfs subvolume create _snapshots && \
-   cd $pth && \
-   umount /mnt && \
-   mount -o subvol=_active/rootvol /dev/mapper/cryptroot /mnt && \
-   mkdir /mnt/home && \
-   mkdir /mnt/tmp && \
-   mkdir /mnt/boot && \
-   mount -o subvol=_active/tmp /dev/mapper/cryptroot /mnt/tmp && \
-   mount /dev/$(echo $DISKNAME2)1 /mnt/boot && \
-   mount -o subvol=_active/homevol /dev/mapper/cryptroot /mnt/home || \
-   return 1
-}
-
-generate_fstab ()
-{
-   mkdir /mnt/etc && \
-   echo UUID=$(blkid -s UUID -o value /dev/$(echo $DISKNAME2)1) /boot   vfat  rw,relatime,fmask=0022,dmask=0022,codepage=437,iocharset=iso8859-1,shortname=mixed,utf8,errors=remount-ro   0  2 > /mnt/etc/fstab && \
-   UUID2=$(blkid -s UUID -o value /dev/mapper/cryptroot) || \
-   return 1
-   if [ "${SWAP}" != "n" ]; then echo UUID=$(blkid -s UUID -o value /dev/mapper/cryptswap) swap  swap  defaults 0  0 >> /mnt/etc/fstab; fi
-   if [ "$(lsblk -d -o name,rota | grep $DISKNAME | grep 1 | wc -l)" -eq 1 ]; then
-      echo UUID=$UUID2 /  btrfs rw,relatime,compress=lzo,autodefrag,space_cache=v2,subvol=/_active/rootvol   0  0 >> /mnt/etc/fstab
-      echo UUID=$UUID2 /tmp  btrfs rw,relatime,compress=lzo,autodefrag,space_cache=v2,subvol=/_active/tmp  0  0 >> /mnt/etc/fstab
-      echo UUID=$UUID2 /home btrfs rw,relatime,compress=lzo,autodefrag,space_cache=v2,subvol=/_active/homevol   0  0 >> /mnt/etc/fstab
-      echo UUID=$UUID2 /home/$(echo $USER)/.snapshots btrfs rw,relatime,compress=lzo,autodefrag,space_cache=v2,subvol=/_snapshots 0  0 >> /mnt/etc/fstab
-   else
-      echo UUID=$UUID2 /  btrfs rw,relatime,compress=lzo,ssd,discard,autodefrag,space_cache=v2,subvol=/_active/rootvol   0  0 >> /mnt/etc/fstab
-      echo UUID=$UUID2 /tmp  btrfs rw,relatime,compress=lzo,ssd,discard,autodefrag,space_cache=v2,subvol=/_active/tmp  0  0 >> /mnt/etc/fstab
-      echo UUID=$UUID2 /home btrfs rw,relatime,compress=lzo,ssd,discard,autodefrag,space_cache=v2,subvol=/_active/homevol   0  0 >> /mnt/etc/fstab
-      echo UUID=$UUID2 /home/$(echo $USER)/.snapshots btrfs rw,relatime,compress=lzo,ssd,discard,autodefrag,space_cache=v2,subvol=/_snapshots 0  0 >> /mnt/etc/fstab
-   fi
-}
-
-encryption_key ()
-{
-   mkdir -m 0700 /mnt/etc/keys && \
-   dd if=/dev/urandom bs=1 count=64 of=/mnt/etc/keys/keyfile.bin conv=excl,fsync iflag=fullblock && \
-   chmod 600 /mnt/etc/keys/keyfile.bin && \
-   echo "$PASS" | arch-chroot /mnt cryptsetup luksAddKey /dev/$ROOTNAME /etc/keys/keyfile.bin || \
-   return 1
-   if [ "${SWAP}" != "n" ]; then echo "$PASS" | arch-chroot /mnt cryptsetup luksAddKey /dev/$(echo $DISKNAME2)3 /etc/keys/keyfile.bin; fi
-}
-
-enable_network ()
-{
-   if arch-chroot /mnt readlink /sbin/init | grep systemd >/dev/null; then
-      arch-chroot /mnt systemctl enable NetworkManager || \
-      return 1
-   else
-      ln -sf /etc/sv/dbus /mnt/etc/runit/runsvdir/default/ && \
-      ln -sf /etc/sv/NetworkManager /mnt/etc/runit/runsvdir/default/ || \
-      return 1
-   fi
-}
-
-set_time ()
-{
-   ln -sf /usr/share/zoneinfo/$(echo $TIME) /mnt/etc/localtime || \
-   return 1
-}
-
-set_hostname ()
-{
-   echo $HOST > /mnt/etc/hostname && \
-   echo "127.0.0.1   localhost" > /mnt/etc/hosts && \
-   echo "::1   localhost" >> /mnt/etc/hosts && \
-   echo "127.0.1.1   $(echo $HOST).localdomain  $HOST" >> /mnt/etc/hosts || \
-   return 1
-}
-
-set_password ()
-{
-   printf "$PASS\n$PASS\n" | arch-chroot /mnt passwd && \
-   printf "$PASS\n$PASS\n" | arch-chroot /mnt passwd $USER || \
-   return 1
-}
-
-clean_up ()
-{
-   pacman -Q | awk '{print $1}' > post.txt
-   [ "$(diff pre.txt post.txt | wc -l)" -gt 0 ] && pacman -R $(diff pre.txt post.txt | grep ">" | awk '{print $2}') --noconfirm >/dev/null 2>&1
-   rm pre.txt post.txt && \
-   umount -l /mnt/boot && \
-   umount -Al /dev/mapper/cryptroot && \
-   cryptsetup close /dev/mapper/cryptroot || \
-   return 1
-}
-
-check_error ()
-{
+function check_error() {
    if [ $? -ne 0 ]; then
       echo $1
       exit -1
    fi
 }
 
-pre_checks
+function before_command() {
+   case "$BASH_COMMAND" in
+      $PROMPT_COMMAND)
+	 ;;
+      echo)
+	 ;;
+      printf)
+	 ;;
+      *)
+	 if [ $MANIFEST_MODE = true ]; then # Dry-run mode
+            echo $BASH_COMMAND
+            return
+	 fi
+	 if ! command -v $BASH_COMMAND 2>&1 >/dev/null; then
+            echo "$BASH_COMMAND isn't available on this system. Please install it before proceeding."
+            exit 1
+	 else
+            echo $BASH_COMMAND
+	 fi
+	 ;;
+   esac
+}
 
-[ -z "$1" ] && wget -O prompts.sh https://raw.github.com/EmperorPenguin18/linux-installer/main/prompts.sh || wget -O prompts.sh "$1"
-source ./prompts.sh
+function after_command() {
+   # Nothing
+}
+
+#function arch-chroot() {
+   #TODO
+   # Mimic arch-chroot
+   # sbin thing
+   # qemu-static
+#}
+
+######################################################################
+######################## Common functions ############################
+######################################################################
+
+function parse_args() {
+   OPTIND=1
+   MANIFEST_MODE=false
+   while getopts "h?m:" opt; do
+      case "$opt" in
+         h|\?)
+            echo "This is a help message"
+            exit 0
+            ;;
+         m)  MANIFEST_MODE=true
+            ;;
+      esac
+   done
+   shift $((OPTIND-1))
+   [ "${1:-}" = "--" ] && shift
+   PROMPT_FILE="$1"
+}
+
+function pre_checks() {
+   trap before_command DEBUG
+   PROMPT_COMMAND=after_command
+   if [ "$(id -u)" -ne 0 ]; then
+      echo "This script must be run as root" 
+      exit 1
+   fi
+   # TODO: will want to check for every needed command before starting
+   PROMPT_DISTRO=""
+   PROMPT_EFI=""
+   PROMPT_SECURE=""
+   PROMPT_DISKNAME=""
+   PROMPT_FSTYPE=""
+   PROMPT_ENCRYPT=""
+   PROMPT_PASS=""
+   PROMPT_USER=""
+   PROMPT_HOST=""
+   PROMPT_ARCH=""
+   PROMPT_VARIANT=""
+   PROMPT_TARGET=""
+   if ! ping -q -c 1 -W 1 google.com >/dev/null; then
+      echo "The network is down"
+      exit 1
+   fi
+   PROJECT_URL="https://raw.github.com/EmperorPenguin18/linux-installer/main"
+}
+
+function prompt_verify() {
+   [ -z "${PROMPT_DISTRO}" ] && echo "Distro not set" && exit 1
+   [ -z "${PROMPT_EFI}" ] && echo "EFI choice not set" && exit 1
+   [ -z "${PROMPT_SECURE}" ] && echo "Secure wipe choice not set" && exit 1
+   [ -z "${PROMPT_DISKNAME}" ] && echo "Target diskname not set" && exit 1
+   [ -z "${PROMPT_FSTYPE}" ] && echo "Filesystem type not set" && exit 1
+   [ -z "${PROMPT_ENCRYPT}" ] && echo "Disk encryption choice not set" && exit 1
+   [ -z "${PROMPT_PASS}" ] && echo "Password not set" && exit 1
+   [ -z "${PROMPT_USER}" ] && echo "Username not set" && exit 1
+   [ -z "${PROMPT_HOST}" ] && echo "Hostname not set" && exit 1
+   [ -z "${PROMPT_ARCH}" ] && echo "Target architecture not set" && exit 1
+   [ -z "${PROMPT_VARIANT}" ] && echo "Distro variant not set" && exit 1
+   [ -z "${PROMPT_TARGET}" ] && echo "Boot target not set" && exit 1
+}
+
+function secure_wipe() {
+   if [ "$PROMPT_SECURE" = true ]; then
+      echo "Doing a secure wipe of disk..."
+      cryptsetup open --type plain --key-file /dev/urandom --sector-size 4096 /dev/$PROMPT_DISKNAME && \
+      dd if=/dev/zero of=/dev/$PROMPT_DISKNAME status=progress bs=1M
+   fi
+}
+
+function partition_drive() {
+   if [ "${PROMPT_EFI}" = true ]; then
+      parted --script /dev/$PROMPT_DISKNAME \
+         mklabel gpt \
+         mkpart boot fat32 1MB 521MB \
+         set 1 esp on \
+         mkpart root $PROMPT_FSTYPE 521MB -1s || \
+      return 1
+   else
+      parted --script /dev/$PROMPT_DISKNAME \
+         mklabel msdos \
+         mkpart primary fat32 1MB 521MB \
+         set 1 boot on \
+         mkpart primary $PROMPT_FSTYPE 521MB -1s || \
+      return 1
+   fi
+   local partitions=$(awk "!/0 / && /$PROMPT_DISKNAME/ {print \$4}" /proc/partitions)
+   BOOTNAME=$(echo $partitions | cut -f 0 -d '\n')
+   ROOTNAME=$(echo $partitions | cut -f 1 -d '\n')
+}
+
+function encrypt_partitions() {
+   if [ "$PROMPT_ENCRYPT" = true ]; then
+      echo "$PROMPT_PASS" | cryptsetup -q luksFormat /dev/$ROOTNAME && \
+      echo "$PROMPT_PASS" | cryptsetup open /dev/$ROOTNAME $ROOTNAME && \
+      ROOTNAME=mapper/$ROOTNAME || \
+      return 1
+   fi
+}
+
+function format_partitions() {
+   mkfs.fat -F 32 /dev/$BOOTNAME && \
+   mkfs.$PROMPT_FSTYPE /dev/$ROOTNAME || \
+   return 1
+}
+
+function mount_subvolumes() {
+   mkdir -p /tmp/mnt && \
+   mount -o defaults /dev/$ROOTNAME /tmp/mnt || \
+   return 1
+   if [ "$PROMPT_FSTYPE" = "btrfs" ]; then
+      local opts="defaults,compress=lzo" && \
+      btrfs subvolume create /tmp/mnt/@root && \
+      btrfs subvolume create /tmp/mnt/@home && \
+      btrfs subvolume create /tmp/mnt/@snapshots && \
+      btrfs subvolume create /tmp/mnt/@var_log && \
+      btrfs subvolume set-default $(btrfs subvolume list -p /tmp/mnt | awk '/@root/ {print $2}') / && \
+      umount /tmp/mnt && \
+      mount -o $opts /dev/$ROOTNAME /tmp/mnt && \
+      mkdir -p /tmp/mnt/home && \
+      mount -o $opts,subvol=@home /dev/$ROOTNAME /tmp/mnt/home && \
+      mkdir -p /tmp/mnt/var/log && \
+      mount -o $opts,subvol=@var_log /dev/$ROOTNAME /tmp/mnt/var/log || \
+      return 1
+   fi
+   mkdir -p /tmp/mnt/boot && \
+   mount -o defaults /dev/$BOOTNAME /tmp/mnt/boot || \
+   return 1
+}
+
+function generate_fstab() {
+   mkdir -p /tmp/mnt/etc || \
+   return 1
+   for I in $(grep "/tmp/mnt" /proc/self/mountinfo); do
+      echo $I | awk '{print $10 $5 $9 $11}' >> /tmp/mnt/etc/fstab || \
+      return 1
+   done
+}
+
+function print_logo() {
+   local screenfetch=$(curl -s "https://git.io/vaHfR") && \
+   echo $screenfetch | sh -s -- -D "${PROMPT_DISTRO}" -L || \
+   return 1
+   #echo $screenfetch | sh -s -- -D "${PROMPT_DISTRO}"-linux -L
+}
+
+function create_user() {
+   useradd -R /tmp/mnt -m -G video $PROMPT_USER || \
+   return 1
+}
+
+function set_hostname () {
+   echo $PROMPT_HOST > /tmp/mnt/etc/hostname || \
+   return 1
+}
+
+function set_password() {
+   printf "$PROMPT_PASS\n$PROMPT_PASS\n" | passwd -R /tmp/mnt && \
+   printf "$PROMPT_PASS\n$PROMPT_PASS\n" | passwd -R /tmp/mnt $USER || \
+   return 1
+}
+
+function clean_up() {
+   umount -R /tmp/mnt || \
+   return 1
+   if [ "$PROMPT_ENCRYPT" = true ]; then
+      cryptsetup close /dev/$ROOTNAME || \
+      return 1
+   fi
+}
+
+######################################################################
+################ Distro-specific functions (Arch) ####################
+######################################################################
+
+function install_packages() {
+   local file="/tmp/mnt/$PROMPT_DISTRO.qcow2" && \
+   curl -s "https://jenkins.linuxcontainers.org/job/image-$PROMPT_DISTRO/architecture=$PROMPT_ARCH,release=current,variant=$PROMPT_VARIANT/lastSuccessfulBuild/artifact/disk.qcow2" > "$file" && \
+   virt-copy-out -a "$file" /tmp/mnt && \
+   rm -f "$file" || \
+   return 1
+}
+
+function create_bootloader() {
+   echo "GRUB_ENABLE_CRYPTODISK=y" >> /tmp/mnt/etc/default/grub && \
+   grub-install -d /tmp/mnt/usr/lib/grub --boot-directory=/tmp/mnt/boot --target=$PROMPT_TARGET --bootloader-id=GRUB /dev/$PROMPT_DISKNAME --recheck --removable && \
+   grub-mkconfig -o /tmp/mnt/boot/grub/grub.cfg || \
+   return 1
+   #arch-chroot /mnt dracut --force --regenerate-all || \
+}
+
+function distro_clean() {
+   # Nothing
+}
+
+######################################################################
+#################### Actual script begins ############################
+######################################################################
+
+echo "-------------------------------------------------"
+echo "               Initializing script               "
+echo "-------------------------------------------------"
+parse_args
+pre_checks
+[ -z "${PROMPT_FILE}" ] && \
+curl -sL $PROJECT_URL/prompts.sh | . /dev/stdin || \
+. $PROMPT_FILE
 user_prompts
+prompt_verify
 
 echo "-------------------------------------------------"
 echo "                Partitioning disk                "
 echo "-------------------------------------------------"
-setup_partitions
+secure_wipe
+check_error "Secure wipe failed"
 partition_drive
 check_error "Partition drive failed"
 encrypt_partitions
 check_error "Encrypt partitions failed"
+
 echo "-------------------------------------------------"
 echo "              Formatting partitions              "
 echo "-------------------------------------------------"
@@ -232,28 +278,19 @@ format_partitions
 check_error "Format partitions failed"
 mount_subvolumes
 check_error "Mount subvolumes failed"
-generate_fstab
-check_error "Generate fstab failed"
+
 echo "-------------------------------------------------"
 echo "                Installing distro                "
 echo "-------------------------------------------------"
-
-wget -O $(echo $DISTRO).sh https://raw.github.com/EmperorPenguin18/linux-installer/main/distros/$(echo $DISTRO).sh
-source ./$(echo $DISTRO).sh
-print_logo
+print_logo $PROMPT_DISTRO
 check_error "Print logo failed"
+DISTRO_SCRIPT=$(curl -sL $PROJECT_URL/distros/$PROMPT_DISTRO.sh)
+[ -z "${DISTRO_SCRIPT}" ] && echo "Unsupported distro requested" && exit 1 || \
+echo $DISTRO_SCRIPT | . /dev/stdin
 install_packages
 check_error "Install packages failed"
-set_locale
-check_error "Set locale failed"
-encryption_key
-check_error "Encryption key failed"
-create_user
-check_error "Create user failed"
-set_initramfs
-check_error "Set initramfs failed"
-enable_network
-check_error "Enable network failed"
+generate_fstab
+check_error "Generate fstab failed"
 create_bootloader
 check_error "Create bootloader failed"
 distro_clean
@@ -262,8 +299,8 @@ check_error "Distro clean failed"
 echo "-------------------------------------------------"
 echo "                  Finishing up                   "
 echo "-------------------------------------------------"
-set_time
-check_error "Set time failed"
+create_user
+check_error "Create user failed"
 set_hostname
 check_error "Set hostname failed"
 set_password
--- prompts.sh
+++ prompts.sh
@@ -1,42 +1,21 @@
+#!/bin/sh
+
 #linux-installer by Sebastien MacDougall-Landry
 #License is available at
 #https://github.com/EmperorPenguin18/linux-installer/blob/main/LICENSE
 
-user_prompts ()
-{
-   mv /usr/share/zoneinfo/right /usr/share/right
-   mv /usr/share/zoneinfo/posix /usr/share/posix
-   lsblk | awk '/disk/ {print $1 " " $4 " off"}' > disks.txt
-   find /usr/share/zoneinfo -type f | sed 's|/usr/share/zoneinfo/||' | sed -e 's/$/ "" off/' > zones.txt
-   TEMP=$(dialog --stdout \
-      --msgbox "Welcome to linux-installer! Please answer the following questions to begin." 0 0 \
-      --and-widget --clear --radiolist "Choose disk to install to." 0 0 $(wc -l < disks.txt) --file disks.txt \
-      --and-widget --clear --radiolist "What distro do you want to install?" 0 0 0 arch "" on debian "" off fedora "" off opensuse "" off void "" off \
-      --and-widget --clear --radiolist "Choose a timezone." 0 0 $(wc -l < zones.txt) --file zones.txt \
-      --and-widget --clear --inputbox "What will the hostname of this computer be?" 0 0 \
-      --and-widget --clear --inputbox "Enter your username." 0 0 \
-      --and-widget --clear --passwordbox "Enter your password." 0 0 \
-      --and-widget --clear --passwordbox "Confirm password." 0 0 \
-   )
-   rm disks.txt zones.txt
-   if [ "$(echo $TEMP | awk '{print $6}')" != "$(echo $TEMP | awk '{print $7}')" ]; then echo "Passwords do not match"; exit 1; fi
-   DISKNAME=$(echo $TEMP | awk '{print $1}')
-   DISTRO=$(echo $TEMP | awk '{print $2}')
-   TIME=$(echo $TEMP | awk '{print $3}')
-   HOST=$(echo $TEMP | awk '{print $4}')
-   USER=$(echo $TEMP | awk '{print $5}')
-   PASS=$(echo $TEMP | awk '{print $6}')
-   if dialog --yesno "Do you want hibernation enabled (Swap partition)" 0 0; then
-      SWAP=y
-   else
-      SWAP=n
-   fi
-   if dialog --default-button "no" --yesno "This will delete all data on selected storage device. Are you sure you want to continue?" 0 0; then
-      SURE=y
-   else
-      exit 1
-   fi
-   mv /usr/share/right /usr/share/zoneinfo/right
-   mv /usr/share/posix /usr/share/zoneinfo/posix
+function user_prompts() {
+   PROMPT_DISTRO="arch"
+   PROMPT_EFI="no"
+   PROMPT_SECURE="no"
+   PROMPT_DISKNAME="vda"
+   PROMPT_FSTYPE="ext4"
+   PROMPT_ENCRYPT="no"
+   PROMPT_PASS="password"
+   PROMPT_USER="testuser"
+   PROMPT_HOST="testcomputer"
+   PROMPT_ARCH="x86_64"
+   PROMPT_VARIANT="default"
+   PROMPT_TARGET="i386-pc"
    clear
 }
